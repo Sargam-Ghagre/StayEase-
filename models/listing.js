fdc1af0356322f5c0d9d7b2bfe758f7815efb3fe
Sargam Ghagre <sargamghagre19@gmail.com>
2026-03-22 14:30:42 +0530

    Create listing schema with validation and defaults
    Define a Mongoose schema for listings with validation and default values.

--- models/listing.js
+++ models/listing.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/a-river-running-through-a-lush-green-park-Bz5iQD532mg",
+        set: (v) => v=== "" ? "https://unsplash.com/photos/a-river-running-through-a-lush-green-park-Bz5iQD532mg" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
